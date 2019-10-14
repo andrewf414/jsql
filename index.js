38f906025af7b13b2e2a1c54688c1f75d716ed82
Andrew Fitzgerald <afitzgerald@huonaqua.com.au>
2019-10-14 16:45:44 +1100

    seem to have filters working

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 "use strict";
 
 function select(query, data) {
-    let selectIdx = query.toUpperCase().indexOf('SELECT');
-    let fromIdx = query.toUpperCase().indexOf('FROM');
-    let whereIdx = query.toUpperCase().indexOf('WHERE');
-    let orderIdx = query.toUpperCase().indexOf('ORDER BY');
-
-    if (selectIdx < 0 || fromIdx < 0) throw new Error('invalid SQL syntax. No SELECT or FROM found');
-
+    // Get data source table (FROM)
+    let fromIdx = query.toUpperCase().indexOf('FROM', 6);
+    if (fromIdx < 0 ) throw new Error('invalid SQL syntax. No FROM found');
     const table = query.substring(fromIdx + 4, query.indexOf(' ', fromIdx + 5)).trim();
-    const fields = query.substring(selectIdx + 6, fromIdx).split(',').map(field => field.trim());
-    if (fields.includes('*')) fields.push(...Object.keys(data[table][0]));
     
-    const conditions = query.substring(whereIdx + 5, orderIdx === -1 ? query.length : orderIdx).split(' AND ').map(field => field.trim());
-    // const order = orderIdx === -1 ? 'ASC' : query.substring(orderIdx + 8, query.length).trim();
+    // Get fields we are selecting
+    let selectIdx = query.toUpperCase().indexOf('SELECT');
+    if (selectIdx < 0 ) throw new Error('invalid SQL syntax. No SELECT found');
+    let fieldsRgx = /(\s([\w*]+[,\s]+)+)(?=FROM)/gi;
+    const fields = query.match(fieldsRgx)[0].split(',').map(f=>f.trim());
+    if (fields.includes('*')) fields.push(...Object.keys(data[table][0])); 
 
-    console.log(fields);
+    // Get any conditions
+    let re = /(\s\w+\s)(?:<>)(\s[\w\d]+)|(\s\w+\s)(?:<=)(\s[\w\d]+)|(?:>=)(\s[\w\d]+)|(\s\w+\s)[=><](\s[\w\d]+)|(\s\w+\s)like(\s[\w"%]+)|(\s\w+\s)in(\s\(.+\))|(\s\w+\s)(between)(\s[\w\d]+){3}/gi;
+    const conditions = query.match(re).map(c=>c.trim());
+
+    // Get order
+    let orderRgx = query.match(/ORDER BY\s\w+\s?(ASC|DESC)?/i);
+    let order = {};
+    if (orderRgx === null) {
+        // default
+        let i = 0;
+        while (fields[i] === '*') {   
+            i++;
+        }
+        order.field = fields[i];
+        order.direction = 'ASC';
+    } else {
+        order.field = orderRgx[0].substring(orderRgx[0].lastIndexOf(' ')).trim();
+        if (orderRgx[1] === undefined) order.direction = 'ASC'; else order.direction = orderRgx[1];
+    }
+
+    // console.log(fields);
     // console.log(table);
-    console.log(conditions);
-    // console.log(order)
+    // console.log(conditions);
+    // console.log(order);
 
     let filters = [];
     conditions.forEach(c => {
         filters.push(getCondition(c));
-    })
-    // console.log(filters)
+    });
+    console.log(filters);
 
 
     // Do the filtering and return result
@@ -40,24 +58,22 @@ function select(query, data) {
  * @param {string} conditionString e.g. a = b
  */
 function getCondition(conditionString) {
-    const conditionOperators = /(?:<>)|(?:<=)|(?:>=)|[=><]|like|in|between/gi
+    let elements = conditionString.split(/\s+/);
 
-    const comparison = conditionString.match(conditionOperators)[0].trim();
-    const tmp = conditionString.split(conditionOperators);
-
-    if (comparison.toLowerCase() === 'between') {
+    if (elements[1].toLowerCase() === 'between') {
         // need to get two values
+        return { field: elements[0], comparison: elements[1], value1: !isNaN(+elements[2]) ? elements[2] : +elements[2], value2: !isNaN(+elements[4]) ? elements[4] : +elements[4] };
     }
-    if (comparison.toLowerCase() === 'in') {
+    if (elements[1].toLowerCase() === 'in') {
         // get the list
+        console.log(conditionString.match(/\([\d\w,\s]+\)/)[0])
+        return { field: elements[0], comparison: elements[1], value1: conditionString.match(/\([\d\w,\s]+\)/)[0] };
     }
-
-    
-    const field = tmp[0].trim();
-    let value = tmp[1].trim().replace(/["']/g, '');
-    if (!isNaN(+value)) value = +value;
-
-    return { field: field, value: value, comparison: comparison };
+    if (elements[1].toLowerCase() === 'like') {
+        // get the expression
+        return { field: elements[0], comparison: elements[1], value1: elements[2].replace(/["']/g, '') };
+    }
+    return { field: elements[0], comparison: elements[1], value1: !isNaN(+elements[2]) ? elements[2] : +elements[2] };
 }
 
 /**
@@ -72,31 +88,32 @@ function filterAndMapData(data, fields, filters) {
         filters.forEach(f => {
             switch (f.comparison.toLowerCase()) {
                 case '=':
-                    pass *= +(row[f.field] === f.value);
+                    pass *= +(row[f.field] === f.value1);
                     break;
                 case '<>':
-                    pass *= +(row[f.field] !== f.value);
+                    pass *= +(row[f.field] !== f.value1);
                     break;
                 case '>':
-                    pass *= +(row[f.field] > f.value);
+                    pass *= +(row[f.field] > f.value1);
                     break;
                 case '<':
-                    pass *= +(row[f.field] < f.value);
+                    pass *= +(row[f.field] < f.value1);
                     break;
                 case '<=':
-                    pass *= +(row[f.field] <= f.value);
+                    pass *= +(row[f.field] <= f.value1);
                     break;
                 case '>=':
-                    pass *= +(row[f.field] >= f.value);
+                    pass *= +(row[f.field] >= f.value1);
                     break;
                 case 'like':
-                    pass *= +(row[f.field] >= f.value);
+                    let re = new RegExp(`${f.value1.charAt(0) === '%' ? '.*' : '^'}${f.value1}${f.value1.charAt(f.value1.length - 1) === '%' ? '.*' : '$'}`)
+                    pass *= +(row[f.field].match(re) !== null);
                     break;
                 case 'in':
-                    pass *= +(f.value.includes(row[f.field]));
+                    pass *= +(f.value1.replace(/[\(\)]/g, '').split(/\s*,\s*/).includes(row[f.field].toString()));
                     break;
                 case 'between':
-                    pass *= +(row[f.field] >= f.value);
+                    pass *= +(row[f.field] >= f.value1 && row[f.field] <= f.value2);
                     break;
             }
         });
@@ -123,6 +140,6 @@ let data = [
 ]
 
 
-let q = 'SELECT * FROM data WHERE a IN (2,3,4) AND b BETWEEN 1 AND 3 AND c LIKE "fart"'
+let q = 'SELECT * FROM data WHERE a IN (2,3,4) AND b BETWEEN 1 AND 3 AND c LIKE "poo"'
 let result = select(q, { data: data });
 console.log(result);
